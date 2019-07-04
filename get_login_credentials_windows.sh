c73e0dd84dfb39ef7ceb5f7f46b0c9f48bd2ff88
Neo Lin <neo.lin@mile.cloud>
2019-07-04 16:36:11 +0800

    add a script for get windows credentials

---
+++ get_login_credentials_windows.sh
@@ -0,0 +1,3 @@
+
+gcloud compute reset-windows-password vm-bastionhost --user app_admin --zone us-central1-a
+gcloud compute reset-windows-password vm-securehost --user app_admin --zone us-central1-a
