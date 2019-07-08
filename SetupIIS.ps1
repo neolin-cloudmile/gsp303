7c73b03208a035d750ec310cfa354534bf562109
Neo Lin <neo.lin@mile.cloud>
2019-07-08 16:11:15 +0800

    delete SetupIIS.ps1

--- SetupIIS.ps1
+++
@@ -1,9 +0,0 @@
-Set-ExecutionPolicy Bypass -Scope Process
-
-# Install the IIS services
-import-module servermanager
-
-add-windowsfeature web-server -includeallsubfeature
-
-# Overwrite the IIS web server default web page.
-echo '<!doctype html><html><body><h1>Hello World!</h1></body></html>' > C:\inetpub\wwwroot\index.html
